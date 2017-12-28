11f74d756c58777d005c60af0a87d1423aacd113
Johan Aspeling <aspeling.johan@gmail.com>
2017-12-28 22:46:38 +0200

    Added dotnetcore2 project

---
+++ vega/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace vega.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+
+            //throw new Exception("Test");
+            return View();
+        }
+
+        public IActionResult Error() {
+            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            return View();
+        }
+    }
+}
